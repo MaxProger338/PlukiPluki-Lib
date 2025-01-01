0a08c19f3e866b61c83cb86f0aefd2d6d334c539
MaxProger338 <maxproger338@mail.ru>
2025-01-01 18:19:52 +0700

    docs: (v1.7.2) Creating a new example 'Replacing rows in file'

---
+++ examples/replcacing_rows_in_file/README.md
@@ -0,0 +1,25 @@
+# Пример File Indexing
+
+```bash
+cd examples/replcacing_rows_in_file
+```
+
+```bash
+clang++ -c ../../src/PlukiPluki.cpp -o ../../bin/PlukiPluki.o -I ../../include
+```
+
+```bash
+clang++ -c ../../src/core/FileBase.cpp -o ../../bin/FileBase.o
+```
+
+```bash
+ar rcs ../../bin/PlukiPluki.a ../../bin/*.o 
+```
+
+```bash
+clang++ main.cpp ../../bin/PlukiPluki.a -I ../../include -o dist/app.out
+```
+
+```bash
+./dist/app.out
+```
---
+++ examples/replcacing_rows_in_file/main.cpp
@@ -0,0 +1,34 @@
+#include "PlukiPluki.h"
+
+#include <iostream>
+#include <exception>
+
+using namespace PlukiPlukiLib;
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        PlukiPluki file("data/data.txt", std::ios::in);
+
+        std::cout << file.getAmountRows() << '\n';
+
+        file.reopen(std::ios::app);
+
+        file.setRowByIndex(1, "lol!");
+
+        file.reopen(std::ios::in);
+
+        std::cout << file[1] << '\n';
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(...)
+    {
+        std::cerr << "FUCK YOU!!! (said by MEMORY)" << '\n';
+    }
+
+    return 0;
+}
